9f0aaf5d719882fda74fb3d056f9379a027752a7
Lucas Viga <lucasviga12@gmail.com>
2021-09-21 00:48:49 -0400

    create page post and preview post, signature validate

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,25 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,23 +28,15 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>19 de setembro de 2021</time>
-            <strong>Substituindo a instrução switch por Object Literal</strong>
-            <p>Object Literal é um bom substituto da instrução procedural switch. O switch apresenta alguns problemas — o design do código é bem antigo e procedural; dificulta encontrar um possível bug e podemos esquecer de colocar a instrução break;</p>
-          </a>
-
-          <a href="#">
-            <time>19 de setembro de 2021</time>
-            <strong>Substituindo a instrução switch por Object Literal</strong>
-            <p>Object Literal é um bom substituto da instrução procedural switch. O switch apresenta alguns problemas — o design do código é bem antigo e procedural; dificulta encontrar um possível bug e podemos esquecer de colocar a instrução break;</p>
-          </a>
-
-          <a href="#">
-            <time>19 de setembro de 2021</time>
-            <strong>Substituindo a instrução switch por Object Literal</strong>
-            <p>Object Literal é um bom substituto da instrução procedural switch. O switch apresenta alguns problemas — o design do código é bem antigo e procedural; dificulta encontrar um possível bug e podemos esquecer de colocar a instrução break;</p>
-          </a>
+          {posts.map(post => (
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -47,9 +53,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
